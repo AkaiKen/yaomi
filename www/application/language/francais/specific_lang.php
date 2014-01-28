5bf43d38bd43ace3c8bc3165b12815ede7f3b42d
AkaiKen <akaiken@lamecarlate.net>
2014-01-28 19:42:47 +0100

    click on increment buttons now works ; rarities filter ; search 'œ' and 'æ'

---
+++ www/application/language/francais/specific_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['specific.rarities.title'] = 'Raretés';
+$lang['specific.rarities.mythic'] = 'Mythique';
+$lang['specific.rarities.rare'] = 'Rare';
+$lang['specific.rarities.uncommon'] = 'Peu commune';
+$lang['specific.rarities.common'] = 'Commune';
+$lang['specific.rarities.timeshifted'] = 'Timeshifted';
+
+$lang['specific.colors.title'] = 'Couleurs';
+$lang['specific.colors.white'] = 'Blanc';
+$lang['specific.colors.blue'] = 'Bleu';
+$lang['specific.colors.black'] = 'Noir';
+$lang['specific.colors.red'] = 'Rouge';
+$lang['specific.colors.green'] = 'Vert';
+$lang['specific.colors.others'] = 'Autres';
+$lang['specific.colors.exclusive'] = 'Exclusif';
